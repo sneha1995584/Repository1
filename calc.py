f351568885bd39c5fb72df0ed7d512076e126782
sneha1995584 <56508942+sneha1995584@users.noreply.github.com>
2019-10-13 19:32:50 +0530

    merge con

--- calc.py
+++ calc.py
@@ -6,7 +6,7 @@ def add(x,y):
                 x*y
             def divide(x,y):
                 def add(x,y):
-                        pass
+                        x+y
                     def subtract(x,y):
                             pass
                         def multiply(x,y):
